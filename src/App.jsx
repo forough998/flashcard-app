74622f182ad0b5f76121b0c863df874021d2d6c7
Forough Majidi <foroughmajidi1998@gmail.com>
2025-03-30 20:04:32 +0200

    feat: Update mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import FlashcardForm from "./pages/FlashcardForm.jsx";
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-10">
-      <h1 className="text-3xl font-bold mb-6">Flashcard Learning App</h1>
+      <h1 className="text-3xl font-bold mb-6 whitespace-nowrap">Flashcard Learning App</h1>
 
       {/* Navigation Bar */}
-      <nav className="mb-6">
-        <Link to="/" className="mx-4 text-blue-400 hover:text-blue-600">📚 View Flashcards</Link>
+      <nav className="mb-6 flex flex-col sm:flex-row gap-3 sm:gap-6 items-center">
+        <Link to="/" className="mx-4 text-blue-400 hover:text-blue-600 ">📚 View Flashcards</Link>
         <Link to="/new" className="mx-4 text-green-400 hover:text-green-600">➕ Create Flashcard</Link>
       </nav>
 
